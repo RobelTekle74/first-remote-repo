6fd1d4af1dce0699e6d0cca117559e244812921f
Robel Birkneh <robika@Robels-MacBook-Pro.local>
2018-09-20 21:03:40 -0400

    first repo.

---
+++ script.js
@@ -0,0 +1,11 @@
+function marinaraSauce(cookName){
+    var two = 2;
+    var oneForth = 1/4;
+    console.log("Hey, " +cookName+ "! This is my marinara sauce recipe!");
+    console.log("Heat a midium-large saucepan over medium heat.");
+    console.log("Add" + two + "tspns of oil and 5 garlic cloves, cook until golden about" +two+ "minutes.");
+    console.log("Add 1/4 cup water, 2 cans of crushed tomatoes, and salt and season with black pepper to taste.");
+    console.log("Cover the pot, bring to a bil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log("Stir in" +oneForth+ "cup roughly chopped fresh basil, salt and pepper as needed");
+}
+marinaraSauce("Adam");
